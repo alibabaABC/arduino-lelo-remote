9fa39f53b61aa4c5cfcca73795bf956f731c5187
M. Elizabeth Scott <beth@sifteo.com>
2012-10-17 08:46:55 -0700

    Sketch: Control motor power with analog input

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -3,19 +3,31 @@
 
 LeloRemote remote;
 
-void setup() {
+void setup()
+{
   Serial.begin(9600);
   SPI.begin();
   remote.reset();
 }
 
-void loop() {
-  for (int i = 0; i < 5; i++) {
-    remote.txMotorPower(0x80);
-    delay(100);
-  }
-  for (int i = 0; i < 15; i++) {
-    remote.txMotorPower(0x00);
-    delay(100);
-  }
+void loop()
+{
+  // Read potentiometer input
+  int analog = analogRead(0);
+
+  /*
+   * This defines the maximum and minimum analog values that
+   * will correspond to the maximum and minimum vibration levels.
+   * We have a small margin at either side so that it's possible
+   * to reliably reach both extremes.
+   */
+  const int analogMin = 10;
+  const int analogMax = 1023 - 10;
+
+  // Scale from 0 to MAX_POWER
+  int power = (constrain(analog, analogMin, analogMax) - analogMin)
+    * long(remote.MAX_POWER) / long(analogMax - analogMin);
+  
+  remote.txMotorPower(power);
+  delay(10);
 }
